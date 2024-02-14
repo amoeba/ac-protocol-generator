468e5716afab59ca26f21f2a06499175e72df4d6
Bryce Mecum <petridish@gmail.com>
2024-02-14 10:22:23 -0900

    Tidy repo and add scripts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import path from "path";
 
-import { parse } from "./src/parse";
-import { convert } from "./src/typescript";
+import { parse } from "./parse";
+import { convert } from "./typescript";
 
 export const process = (document: string) => {
   const parsed = parse(document);
@@ -10,8 +10,8 @@ export const process = (document: string) => {
   return converted;
 };
 
-const protocol_file = path.join(__dirname, "protocol.xml");
-const out_file = path.join(__dirname, "generated", "protocol.ts");
+const protocol_file = path.join(__dirname, "..", "protocol", "protocol.xml");
+const out_file = path.join(__dirname, "..", "generated", "protocol.ts");
 
 const input = Bun.file(protocol_file);
 const xml = await input.text();
