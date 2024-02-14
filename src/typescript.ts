6e248691548fcfe4b5c95cddfe437d5e7f2295f1
Bryce Mecum <petridish@gmail.com>
2024-02-13 21:11:20 -0900

    Add buggy first version of types

--- src/typescript.ts
+++ src/typescript.ts
@@ -1,6 +1,6 @@
 import ts from "typescript";
 
-import type { EnumData, EnumValue, ParseResult } from "./types";
+import type { EnumData, EnumValue, ParseResult, TypeData } from "./types";
 import { parseSignedHexString } from "./util.ts"
 
 const createEnumMember = (member: EnumValue) => {
@@ -41,7 +41,52 @@ const createEnum = (enum_data: EnumData) => {
   return node;
 };
 
-function print(nodes: ts.EnumDeclaration[]) {
+const createTypeMember = (field_data: FieldData) => {
+  const id = ts.factory.createIdentifier(field_data.name);
+  const nameProp = ts.factory.createPropertySignature(
+    undefined,
+    id,
+    undefined,
+    ts.factory.createTypeReferenceNode("string")
+  );
+
+  if (field_data.comment) {
+    ts.addSyntheticLeadingComment(
+      nameProp,
+      ts.SyntaxKind.MultiLineCommentTrivia,
+      field_data.comment,
+      true,
+    );
+  }
+
+  return nameProp;
+}
+
+const createType = (type_data: TypeData): ts.InterfaceDeclaration => {
+  const id = ts.factory.createIdentifier(type_data.name);
+  const members = type_data.fields.map(f => createTypeMember(f));
+
+  const interface_decl = ts.factory.createInterfaceDeclaration(
+    /*modifiers*/undefined,
+    /*name*/type_data.name,
+    /*typeParameters*/undefined,
+    /*heritageClauses*/undefined,
+    members
+  )
+
+  if (type_data.comment) {
+    ts.addSyntheticLeadingComment(
+      interface_decl,
+      ts.SyntaxKind.MultiLineCommentTrivia,
+      type_data.comment,
+      true,
+    );
+  }
+
+  return interface_decl;
+}
+
+function print(nodes: ts.NodeArray<ts.Node>) {
   const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
   const resultFile = ts.createSourceFile(
 		/*does nothing?*/ "temp.ts",
@@ -54,4 +99,11 @@ function print(nodes: ts.EnumDeclaration[]) {
   return printer.printList(ts.ListFormat.MultiLine, nodes, resultFile);
 }
 
-export const convert = (result: ParseResult): string => print(result.enums.map((e: EnumData) => createEnum(e)));
+export const convert = (result: ParseResult): string => {
+  const enums = result.enums.map((e: EnumData) => createEnum(e));
+  const types = result.types.map((t: TypeData) => createType(t));
+
+  const x = [...enums, ...types];
+
+  return print(x)
+}
